0db84e6c560d4bfa92c240c0bca8a36578f6b36b
Uthman Dantata <uthmandantata8@gmail.com>
2025-02-11 20:34:12 +0100

    update user

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/auth/user.controller.js";
+import { getUser, loginUser, logoutsUser, registerUser, updateUser } from "../controllers/auth/user.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/logout', logoutsUser);
+router.get('/user', protect, getUser);
+router.post('/update-user', protect, updateUser);
 
 export default router;
